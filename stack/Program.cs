06a7680489fbd36490ae59b77acefdc5def0b44a
ceren <ceren.karahasan44@gmail.com>
2025-05-09 20:14:23 +0300

    stack eklendi

---
+++ stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MessageApp
+{
+    public class Stack<T>
+    {
+        private LinkedList<T> items;
+
+        public Stack()
+        {
+            items = new LinkedList<T>();
+        }
+
+        public void Push(T item)
+        {
+            items.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Mesaj yok.");
+
+            T value = items.First.Value;
+            items.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Mesaj yok.");
+
+            return items.First.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return items.Count == 0;
+        }
+
+        public int Count()
+        {
+            return items.Count;
+        }
+    }
+}
